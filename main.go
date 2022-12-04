01152654066de7ea4ef31facddff66157e084879
Bruce Neco <bruce.neco3@outlook.com>
2022-12-04 17:56:27 -0300

    feat: add products handler to serve mux and add some comments

--- main.go
+++ main.go
@@ -12,23 +12,32 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	// Handlers
 	hh := handlers.NewHello(l)
+	ph := handlers.NewProducts(l)
 
+	// Create serve mux and register the handlers
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/products", ph)
 
+	// create server
 	s := http.Server{Addr: ":9090",
 		Handler:      sm,
 		IdleTimeout:  2 * time.Minute,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
+	// start server
 	go func() {
 		err := s.ListenAndServe()
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
+	log.Printf("Server started on port %s\n", s.Addr)
+
+	// trap sigterm or interrupt and gracefully shutdown the server
 	sigChan := make(chan os.Signal)
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, os.Kill)
